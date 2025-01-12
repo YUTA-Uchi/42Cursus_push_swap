538e435765ae972dc3379468b1c40cdaa9610c76
Yuta Uchiyama <42081576+YUTA-Uchi@users.noreply.github.com>
2025-01-12 19:11:27 +0900

    norm and validate

--- push_swap.h
+++ push_swap.h
@@ -1,12 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yuuchiya <yuuchiya@student.42tokyo.jp>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/12 19:04:26 by yuuchiya          #+#    #+#             */
+/*   Updated: 2025/01/12 19:04:26 by yuuchiya         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 
 #ifndef PUSH_SWAP_H
 # define PUSH_SWAP_H
 
 # include <stdlib.h>
+# include <stdbool.h>
+# include <limits.h>
 
 typedef struct s_stack
 {
-	int	content;
+	int		content;
 	t_stack	*next;
 }	t_stack;
 
