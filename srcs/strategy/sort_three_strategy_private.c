84e171dda31dfd9530adafcc48437b07b86c6211
Yuta Uchiyama <42081576+YUTA-Uchi@users.noreply.github.com>
2025-02-10 19:51:06 +0900

    go home

--- srcs/strategy/sort_three_strategy_private.c
+++ srcs/strategy/sort_three_strategy_private.c
@@ -6,7 +6,7 @@
 /*   By: yuuchiya <yuuchiya@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/05 15:54:18 by yuuchiya          #+#    #+#             */
-/*   Updated: 2025/02/10 14:57:25 by yuuchiya         ###   ########.fr       */
+/*   Updated: 2025/02/10 19:26:49 by yuuchiya         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -67,20 +67,20 @@ void	sort_three_minimum(t_sort_solver *solver)
 		solver->ops->rra(solver->stack_a);
 }
 
-void	sort_three(t_sort_solver *solver)
+void	sort_three(t_sort_solver *solver, t_stack_pos pos)
 {
-	int	distance_to_min;
+	int	distance_to_max;
 
 	if (solver->stack_a->size == 3)
 	{
 		sort_three_minimum(solver);
 		return ;
 	}
-	distance_to_min = get_distance_to_min(solver->stack_a, 3);
-	if (distance_to_min == 0)
+	move_to_top_a(solver, pos);
+	distance_to_max = get_distance_to_max(solver->stack_a, 3);
+	if (distance_to_max == 0)
 		sort_three_distance_0(solver);
-	else if (distance_to_min == 1)
+	else if (distance_to_max == 1)
 		sort_three_distance_1(solver);
-	else
-		sort_three_distance_2(solver);
+	sort_two(solver, TOP_A);
 }
--- srcs/strategy/v3_quick_sort_strategy_private_position.c
+++ srcs/strategy/v3_quick_sort_strategy_private_position.c
@@ -6,7 +6,7 @@
 /*   By: yuuchiya <yuuchiya@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/10 18:52:57 by yuuchiya          #+#    #+#             */
-/*   Updated: 2025/02/10 18:53:17 by yuuchiya         ###   ########.fr       */
+/*   Updated: 2025/02/10 19:50:24 by yuuchiya         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -47,4 +47,25 @@ t_stack_pos	get_min_pos(t_stack_pos pos)
 		return (BOTTOM_B);
 	else
 		return (TOP_B);
+}
+
+void	move_to_top_a(t_sort_solver *solver, t_stack_pos pos)
+{
+	if (pos == TOP_A)
+	{
+		solver->ops->ra(solver->stack_a);
+	}
+	else if (pos == BOTTOM_A)
+	{
+		solver->ops->rra(solver->stack_a);
+	}
+	else if (pos == TOP_B)
+	{
+		solver->ops->pa(solver->stack_a, solver->stack_b);
+	}
+	else if (pos == BOTTOM_B)
+	{
+		solver->ops->rrb(solver->stack_b);
+		solver->ops->pa(solver->stack_a, solver->stack_b);
+	}
 }
